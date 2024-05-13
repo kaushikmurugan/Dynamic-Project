de890255367209c01de3684a2ba9def8de732a11
kaushikmurugan <kaushik.here252@gmail.com>
2024-05-13 14:27:03 +0530

    cart page finished

--- JS/cart.js
+++ JS/cart.js
@@ -1,6 +1,8 @@
-let localcartPull = JSON.parse(localStorage.getItem("localpush"));
-// console.log("localpull", localcartPull);
 
+//localcart pull function
+let localcartPull = JSON.parse(localStorage.getItem("localpush"));
+
+//changing to a new array of function
 let newCartData = [];
 localcartPull.forEach((items) => {
   newCartData.push({
@@ -15,45 +17,8 @@ localcartPull.forEach((items) => {
     price: items.price,
   });
 });
-// console.log("new cart", newCartData);
 
-// newCartData map function starts here
-let innerHTML = "";
-
-newCartData.forEach((item) => {
-  innerHTML += `
-    <div class="card mx-lg-5 m-3" style="width: 15rem;">
-      <img src="${item.img}" alt="${item.foodName}" class="card-img-top img-fluid rounded-3" style="background-color: gray;">
-      <div class="card-body text-center">
-        <div class="d-flex justify-content-between align-items-center">
-          <h6 class="card-title text-black text-capitalize"><span class="fname">${item.foodName}</span></h6>
-          <div class="d-flex">
-            <h6 class="badge bg-black">${item.rating}<span class="fa fa-star" style="font-size:0.6rem;"></span></h6>
-          </div>
-        </div>
-        <div class="d-flex justify-content-between">
-          <p>Type <br><span class="mb-0">${item.type}</span></p>
-          <p>Cuisines <br><span class="mb-0">${item.cuisine}</span></p>
-          <p>Price <br><span class="mb-0 price" data-original-price="${item.price}">₹ ${item.price}</span></p>
-        </div>
-        <div class="d-flex justify-content-center">
-          <h6>Brand ${item.brand}</h6>
-        </div>
-        <div class="d-flex justify-content-between align-items-center">
-          <div class="btn btn-primary w-50 Decrement" id="${item.id}">Remove</div>
-          <div class="px-2 fw-bold" id="quantity">${item.quantity}</div>
-          <div class="btn btn-primary w-50 Increment" id="${item.id}">Add</div>
-        </div>
-      </div>
-    </div>
-  `;
-  totalAmount()
-});
-
-// Set the innerHTML of the container with the dynamically generated content
-document.getElementById("cartitemnew").innerHTML = innerHTML;
-
-//rendered items
+//rendered items cart items iteration
 function RenderedItems() {
   let innerHTML = "";
 
@@ -85,72 +50,76 @@ function RenderedItems() {
     </div>
   `;
   });
-}
 
-// Increment program
-document.querySelectorAll(".Increment").forEach((item) => {
-  item.addEventListener("click", IncrementItem);
-});
+  document.getElementById("cartitemnew").innerHTML = innerHTML;
 
-function IncrementItem() {
-  let add = this.id;
-  let quantityElement = this.parentElement.querySelector("#quantity");
-  let quantity = parseInt(quantityElement.innerHTML);
-  let priceElement = this.parentElement.parentElement.querySelector(".price");
-  let originalPriceValue = priceElement.getAttribute("data-original-price");
+  // Increment program
+  document.querySelectorAll(".Increment").forEach((item) => {
+    item.addEventListener("click", IncrementItem);
+  });
 
-  let originalPrice = parseFloat(originalPriceValue);
+  //decrement function
+  document.querySelectorAll(".Decrement").forEach((item) => {
+    item.addEventListener("click", DecrementItem);
+  });
 
-  // Increase quantity
-  quantity++;
-  quantityElement.innerHTML = quantity;
-
-  // Calculate new total price
-  let totalPrice = originalPrice * quantity;
-  priceElement.innerHTML = "₹ " + totalPrice.toFixed(2); // Update the displayed price
   totalAmount();
-  RenderedItems()
 }
+RenderedItems();
 
-//decrement function
-document.querySelectorAll(".Decrement").forEach((item) => {
-  item.addEventListener("click", DecrementItem);
-});
-
-function DecrementItem() {
+//Increment function
+function IncrementItem() {
   let itemId = this.id;
-  let quantityElement = this.parentElement.querySelector("#quantity");
-  let quantity = parseInt(quantityElement.innerHTML);
-  let priceElement = this.parentElement.parentElement.querySelector(".price");
-  let originalPrice = parseFloat(
-    priceElement.getAttribute("data-original-price")
-  );
-  if (quantity > 0) {
-    quantity--;
-    quantityElement.innerHTML = quantity;
-    let totalPrice = originalPrice * quantity;
-    priceElement.innerHTML = "₹ " + totalPrice.toFixed(2); // Update the displayed price
-    if (quantity === 0) {
-      const index = newCartData.findIndex((item) => item.id === itemId);
-      // console.log(index);
-      if (index === -1) {
-        newCartData.splice(index, 1);
-        RenderedItems();
-        totalAmount();
-      }
-    }
-    totalAmount();
+  let addingItem = newCartData.find((item) => item.id == itemId);
+  addingItem.quantity++;
+
+  let newPrice =
+    (addingItem.price * addingItem.quantity -
+      addingItem.price * (addingItem.quantity - 1)) /
+    (addingItem.quantity - 1);
+  addingItem.price = newPrice * addingItem.quantity;
+
+  let quantityElement = document.querySelector(`#quantity`);
+  let priceElement = document.querySelector(`.price`);
+  if (quantityElement && priceElement) {
+    quantityElement.textContent = addingItem.quantity;
+    priceElement.textContent = "₹ " + addingItem.price.toFixed(2);
   }
+
+  // Update the total amount
+  totalAmount();
+  RenderedItems();
 }
 
-let totalamount = "";
+//Decrement function
+function DecrementItem() {
+  let itemToDec =this.id;
+  let removingItem = newCartData.find((element) => element.id == itemToDec);
 
+  if (removingItem.quantity > 1) {
+    let currPrice =
+      (removingItem.price * removingItem.quantity - removingItem.price * (removingItem.quantity - 1)) /
+      removingItem.quantity;
+    removingItem.quantity -= 1;
+    removingItem.price = currPrice * removingItem.quantity;
+  } else {
+    const index = newCartData.findIndex((item) => item.id === removingItem.id);
+    newCartData.splice(index, 1);
+  }
+
+  totalAmount();
+  RenderedItems();
+}
+
+//Total amount display function 
+let totalamount = "";
 function totalAmount() {
-  var sum = 0,quantity = 0;
+  var sum = 0,
+    quantity = 0;
   newCartData.map((item) => {
     quantity += item.quantity;
     sum += item.price;
   });
   document.getElementById("total-item").innerText = quantity;
-  document.getElementById("total-price").innerText = "₹"+sum;
+  document.getElementById("total-price").innerText = "₹" + sum;
 }
--- JS/french cuisine.js
+++ JS/french cuisine.js
@@ -1,122 +1,152 @@
 import { foodItem } from "./fooditem.js";
 
-const frenchCuisineData = foodItem.filter(items => items.cuisine == "FC");
+const frenchCuisineData = foodItem.filter((items) => items.cuisine == "FC");
 console.log(frenchCuisineData);
 
 let topic = document.createElement("h1");
-topic.classList.add("text-center","text-uppercase");
+topic.classList.add("text-center", "text-uppercase");
 let tcontent = document.createTextNode("french cuisine");
 topic.appendChild(tcontent);
-document.getElementById("french").appendChild(topic);
+document.getElementById("frenchitem").appendChild(topic);
 
 let god = document.createElement("div");
 god.classList.add("d-flex", "flex-wrap", "justify-content-center");
-document.getElementById("french").appendChild(god);
+document.getElementById("frenchitem").appendChild(god);
 
 frenchCuisineData.map((item) => {
+  let div = document.createElement("div");
+  div.classList.add("card", "mx-lg-5", "m-3");
+  div.setAttribute("style", "width: 15rem");
+  god.appendChild(div);
 
-    let div = document.createElement("div");
-    div.classList.add("card", "mx-lg-5", "m-3");
-    div.setAttribute("style", "width: 15rem");
-    god.appendChild(div);
+  let img = document.createElement("img");
+  img.setAttribute("src", item.img);
+  img.setAttribute("alt", item.foodName);
+  img.classList.add("card-img-top", "img-fluid", "rounded-3");
+  img.setAttribute("style", "background-color: gray");
+  div.appendChild(img);
 
+  let div2 = document.createElement("div");
+  div2.classList.add("card-body", "text-center");
+  div.appendChild(div2);
 
-    let img = document.createElement("img")
-    img.setAttribute("src", item.img);
-    img.setAttribute("alt", item.foodName);
-    img.classList.add("card-img-top", "img-fluid", "rounded-3");
-    img.setAttribute("style", "background-color: gray")
-    div.appendChild(img);
+  let div3 = document.createElement("div");
+  div3.classList.add("d-flex", "justify-content-between", "align-items-center");
+  div2.appendChild(div3);
 
-    let div2 = document.createElement("div");
-    div2.classList.add("card-body", "text-center");
-    div.appendChild(div2);
+  let h6 = document.createElement("h6");
+  h6.classList.add("card-title", "text-black", "text-capitalize");
+  let span1 = document.createElement("span");
+  span1.setAttribute("id", "fname"); //this is the name id;
+  span1.innerHTML = item.foodName;
+  h6.appendChild(span1);
+  div3.appendChild(h6);
 
-    let div3 = document.createElement("div");
-    div3.classList.add("d-flex", "justify-content-between", "align-items-center");
-    div2.appendChild(div3);
+  let div4 = document.createElement("div");
+  div4.classList.add("d-flex");
+  div3.appendChild(div4);
 
-    let h6 = document.createElement("h6");
-    h6.classList.add("card-title", "text-black", "text-capitalize");
-    let span1 = document.createElement("span");
-    span1.innerHTML = item.foodName;
-    h6.appendChild(span1);
-    div3.appendChild(h6);
+  let h62 = document.createElement("h6");
+  h62.classList.add("badge", "bg-black");
+  let span2 = document.createElement("span");
+  span2.innerText = item.rating;
+  h62.appendChild(span2);
+  let span3 = document.createElement("span");
+  span3.classList.add("fa", "fa-star");
+  span3.setAttribute("style", "font-size:0.6rem;");
+  h62.appendChild(span3);
+  div4.appendChild(h62);
 
-    let div4 = document.createElement("div");
-    div4.classList.add("d-flex");
-    div3.appendChild(div4);
+  let div5 = document.createElement("div");
+  div5.classList.add("d-flex", "justify-content-between");
+  div2.appendChild(div5);
 
-    let h62 = document.createElement("h6");
-    h62.classList.add("badge", "bg-black");
-    let span2 = document.createElement("span");
-    span2.innerText = item.rating;
-    h62.appendChild(span2);
-    let span3 = document.createElement("span");
-    span3.classList.add("fa", "fa-star");
-    span3.setAttribute("style", "font-size:0.6rem;")
-    h62.appendChild(span3);
-    div4.appendChild(h62);
+  let p1 = document.createElement("p");
+  let ptype = document.createTextNode("Type ");
+  p1.appendChild(ptype);
+  div5.appendChild(p1);
 
-    let div5 = document.createElement("div");
-    div5.classList.add("d-flex", "justify-content-between");
-    div2.appendChild(div5);
+  let br = document.createElement("br");
+  p1.appendChild(br);
 
-    let p1 = document.createElement("p");
-    let ptype = document.createTextNode("Type ");
-    p1.appendChild(ptype);
-    div5.appendChild(p1);
+  let span4 = document.createElement("span");
+  span4.classList.add("mb-0");
+  span4.innerText = item.type;
+  p1.appendChild(span4);
 
-    let br = document.createElement("br");
-    p1.appendChild(br)
+  let p2 = document.createElement("p");
+  let pcuisine = document.createTextNode("Cuisines ");
+  p2.appendChild(pcuisine);
+  div5.appendChild(p2);
 
-    let span4 = document.createElement("span");
-    span4.classList.add("mb-0");
-    span4.innerText = item.type;
-    p1.appendChild(span4);
+  let span5 = document.createElement("p");
+  span5.classList.add("mb-0");
+  span5.innerText = item.cuisine;
+  p2.appendChild(span5);
 
-    let p2 = document.createElement("p");
-    let pcuisine = document.createTextNode("Cuisines ");
-    p2.appendChild(pcuisine);
-    div5.appendChild(p2);
+  let p3 = document.createElement("p");
+  let pprice = document.createTextNode("price ");
+  p3.appendChild(pprice);
+  div5.appendChild(p3);
 
-    let span5 = document.createElement("p");
-    span5.classList.add("mb-0");
-    span5.innerText = item.cuisine;
-    p2.appendChild(span5);
+  let span6 = document.createElement("p");
+  span6.classList.add("mb-0");
+  span6.innerText = "₹ " + item.price;
+  p3.appendChild(span6);
 
-    let p3 = document.createElement("p");
-    let pprice = document.createTextNode("price ");
-    p3.appendChild(pprice);
-    div5.appendChild(p3);
+  let div6 = document.createElement("div");
+  div6.classList.add("d-flex", "justify-content-center");
+  div2.appendChild(div6);
 
-    let span6 = document.createElement("p");
-    span6.classList.add("mb-0");
-    span6.innerText = "₹ " + item.price;
-    p3.appendChild(span6);
+  let h63 = document.createElement("h6");
+  let pbrand = document.createTextNode("Brand ");
+  h63.appendChild(pbrand);
+  div6.appendChild(h63);
 
-    let div6 = document.createElement("div");
-    div6.classList.add("d-flex", "justify-content-center");
-    div2.appendChild(div6);
+  let span7 = document.createElement("span");
+  span7.classList.add("text-muted", "text-capitalize");
+  span7.innerText = item.brand;
+  h63.appendChild(span7);
 
-    let h63 = document.createElement("h6");
-    let pbrand = document.createTextNode("Brand ");
-    h63.appendChild(pbrand);
-    div6.appendChild(h63);
+  let div7 = document.createElement("div");
+  div2.appendChild(div7);
 
-    let span7 = document.createElement("span");
-    span7.classList.add("text-muted", "text-capitalize");
-    span7.innerText = item.brand;
-    h63.appendChild(span7);
-
-    let div7 = document.createElement("div");
-    div2.appendChild(div7);
-
-    let add = document.createElement("a");
-    add.setAttribute("href", "#");
-    add.classList.add("btn", "btn-primary");
-    let addtext = document.createTextNode("Add");
-    add.appendChild(addtext);
-    div7.appendChild(add);
-
+  let add = document.createElement("p");
+  add.classList.add("btn", "btn-primary", "add-to-cart");
+  add.setAttribute("id", item.id);
+  let addtext = document.createTextNode("Add");
+  add.appendChild(addtext);
+  div7.appendChild(add);
 });
+
+
+document.querySelectorAll(".add-to-cart").forEach((item) => {
+  item.addEventListener("click", addIt);
+});
+
+let cartData = [];
+
+function addIt() {
+  let addItem = this.id;
+  let divtex = this.innerText;
+  console.log(divtex);
+  let objItem = foodItem.find((item) => item.id == addItem);
+  console.log("object item", objItem);
+  if (divtex == "Add") {
+    cartData.push(objItem);
+    document.getElementById(addItem).innerHTML = "Added";
+    console.log(cartData);
+  } else {
+    const index = cartData.findIndex((element) => element.id == addItem);
+    console.log(index);
+    if (index !== -1) {
+      cartData.splice(index, 1);
+      console.log("yes", "removed");
+      document.getElementById(addItem).innerHTML = "Add";
+      console.log(cartData);
+    }
+  }
+  let localCartPush = JSON.stringify(cartData);
+  localStorage.setItem("localpush", localCartPush);
+  console.log("localpush", localCartPush);
+}
